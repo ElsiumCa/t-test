7d48a3e38a4dbbb2eb151ff45420da80f0b02f40
ElsiumCa <173598971+ElsiumCa@users.noreply.github.com>
2025-05-21 13:22:51 +0300

    name and command lines

--- Ttest.py
+++ Ttest.py
@@ -1,11 +1,13 @@
 import scipy.stats as stats
 
+#Araştırma data'sı
 questdata = [10, 10, 4, 3, 6, 4, 9, 5, 7, 6,
         6, 6, 9, 6, 8, 5, 8, 7, 8, 7,
         8, 10, 9, 7, 6, 4, 5, 4, 7, 9]
 
 test_statistic, p_value = stats.ttest_1samp(questdata, popmean=5)
 
+#tek yünlü p değerinin bölünmesi
 p_value_one_tailed = p_value / 2
 
 print(f"Örneklem ortalaması: {sum(questdata)/len(questdata):.2f}")
